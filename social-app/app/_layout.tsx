7c7445ee44964b97fc49263df8f06b02aaba4ac9
nouribram <nourtesla@yahoo.com>
2025-03-22 13:59:51 +0200

    new update

--- social-app/app/_layout.tsx
+++ social-app/app/_layout.tsx
@@ -1,13 +1,28 @@
 import { Stack } from "expo-router";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { ClerkProvider } from '@clerk/clerk-expo'
+
+
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+if (!publishableKey) {
+  throw new Error(
+    'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env',
+  )
+}
+
+
 
 export default function RootLayout() {
     return(
-    <SafeAreaProvider>
-        <SafeAreaView style={{flex: 1, backgroundColor: "#000"}}>
-          <Stack screenOptions={{ headerShown: false }}/>
-        </SafeAreaView>
-    </SafeAreaProvider>
+      <ClerkProvider publishableKey={publishableKey}>
+        <SafeAreaProvider>
+            <SafeAreaView style={{flex: 1, backgroundColor: "#000"}}>
+              <Stack screenOptions={{ headerShown: false }}/>
+            </SafeAreaView>
+        </SafeAreaProvider>
+    </ClerkProvider>
     );
 
 }
