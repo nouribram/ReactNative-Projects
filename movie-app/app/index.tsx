60314a32915a9f075518ee121bce113763828df6
nouribram <nourtesla@yahoo.com>
2025-05-07 01:44:53 +0300

    new

--- movie-app/app/index.tsx
+++ movie-app/app/index.tsx
@@ -1,9 +1,11 @@
 import { Text, View } from "react-native";
+import {Link} from "expo-router";
 
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
     <Text className="text-5xl font-bold text-dark-200">Nourhan</Text>
+    <Link href="/onboarding">onboarding</Link>
   </View>
   );
 }
