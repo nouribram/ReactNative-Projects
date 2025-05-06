290e829cf1cb51314c8b52dd1135c4350e19269c
nouribram <nourtesla@yahoo.com>
2025-05-06 19:01:36 +0300

    new

--- movie-app/app/index.tsx
+++ movie-app/app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View>
-      <Text>Nourhan</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-600">Hello, Tailwind!</Text>
     </View>
   );
 }
