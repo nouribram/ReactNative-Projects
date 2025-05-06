4d0e99108b3544253c2710a35e7241221fad7476
nouribram <nourtesla@yahoo.com>
2025-05-06 17:51:02 +0300

    new

--- movie-app/app/index.tsx
+++ movie-app/app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>welcome.</Text>
+    <View>
+      <Text>Nourhan</Text>
     </View>
   );
 }
