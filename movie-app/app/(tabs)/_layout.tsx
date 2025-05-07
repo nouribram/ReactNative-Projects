06e1623241745e55e5b1c7187409f2bcd3fbbf35
nouribram <nourtesla@yahoo.com>
2025-05-08 00:31:23 +0300

    new

--- movie-app/app/(tabs)/_layout.tsx
+++ movie-app/app/(tabs)/_layout.tsx
@@ -1,9 +1,37 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, ImageBackground  } from 'react-native'
 import React from 'react'
 import {Tabs} from "expo-router";
 import {images} from "@/constants/images";
 import {icons} from "@/constants/icons";
-import { ImageBackground } from 'react-native';
+
+
+const TabIcon = ({ focused, icon, title}: any) => {
+      
+  if(focused) {
+   
+    return(
+      <ImageBackground 
+               
+      source={images.highlight}
+      className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
+      >
+     <Image source={icon}
+     tintColor="#151312" className="size-5" />
+     <Text className="text-secondary text-base font-semibold ml-2">{title}</Text>
+     </ImageBackground>
+     )
+    
+  }
+
+  return (
+    <View className='size-full justify-center items-center mt-4 rounded-full'>
+      <Image source={icon} tintColor="#A8B5DB" className='size-5'/>
+    </View>
+  )
+
+  
+}
+
 
 const _layout = () => {
   return (
@@ -14,18 +42,12 @@ const _layout = () => {
             title: 'Home',
             headerShown: false,
             tabBarIcon: ({ focused }) => (
-                <>
-                 
-                 <ImageBackground 
-                 
-                 source={images.highlight}
-                 className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
-                 >
-                <Image source={icons.home}
-                tintColor="#151312" className="size-5" />
-                <Text className="text-secondary text-base font-semibold ml-2">Home</Text>
-                </ImageBackground>
-                </>
+              <TabIcon
+               focused={focused} 
+               icon={icons.home} 
+               title="Home" 
+               />
+              
             )
         }}
         />
@@ -34,7 +56,15 @@ const _layout = () => {
           name="search"
           options={{
             title: 'Search',
-            headerShown: false
+            headerShown: false,
+            tabBarIcon: ({ focused }) => (
+              <TabIcon
+               focused={focused} 
+               icon={icons.search} 
+               title="Search" 
+               />
+              
+            )
           }}
        />
 
@@ -42,7 +72,14 @@ const _layout = () => {
           name="saved"
           options={{
             title: 'Saved',
-            headerShown: false
+            headerShown: false,
+            tabBarIcon: ({ focused }) => (
+              <TabIcon
+               focused={focused} 
+               icon={icons.save} 
+               title="Saved" 
+               />
+              )
           }}
        />
 
@@ -50,7 +87,14 @@ const _layout = () => {
           name="profile"
           options={{
             title: 'Profile',
-            headerShown: false
+            headerShown: false,
+            tabBarIcon: ({ focused }) => (
+              <TabIcon
+               focused={focused} 
+               icon={icons.person} 
+               title="Profile" 
+               />
+              )
           }}
        />
 
