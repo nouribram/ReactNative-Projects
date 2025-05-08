1f58368ac046e5832b8af100ef32275837edc805
nouribram <nourtesla@yahoo.com>
2025-05-08 10:14:51 +0300

    new update

---
+++ movie-app/components/SearchBar.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SearchBar = () => {
+  return (
+    <View className='flex-row'>
+      <Text>SearchBar</Text>
+    </View>
+  )
+}
+
+export default SearchBar
