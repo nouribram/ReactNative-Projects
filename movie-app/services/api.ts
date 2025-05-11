6c8f77805bfe661c2ae2796374e3a616d45eb29f
nouribram <nourtesla@yahoo.com>
2025-05-11 17:19:12 +0300

    new

---
+++ movie-app/services/api.ts
@@ -0,0 +1,8 @@
+export const TMDB_CONFIG ={
+    BASE_URL: '',
+    API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`
+    }
+}
