47d839361a2cef157ae37823182df35a7a115953
nouribram <nourtesla@yahoo.com>
2025-05-11 17:48:58 +0300

    new

--- movie-app/services/api.ts
+++ movie-app/services/api.ts
@@ -6,3 +6,24 @@ export const TMDB_CONFIG ={
         Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`
     }
 }
+
+export const fetchPopularMovies = async ({ query }: { query: string }) => {
+
+    const endpoint = 
+    
+    '/discover/movie?sort_by=popularity.desc';
+
+    const response = await fetch(endpoint, {
+        method: 'GET',
+        headers: TMDB_CONFIG.headers,
+    });
+
+    if(!response.ok){
+
+        throw new Error('Failed to fetch movies', response.statusText);
+    }
+
+    const data = await response.json();
+
+    return data.results;
+}
