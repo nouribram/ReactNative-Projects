71ca2fe95d143ed9b672c876ab141f0fa9f64078
nouribram <nourtesla@yahoo.com>
2025-05-19 01:28:08 +0300

    new

---
+++ movie-app/services/appwrite.ts
@@ -0,0 +1,20 @@
+// track the searches made by a user
+const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
+
+const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
+
+const client = new Client()
+.setEndpoint('https://cloud.appwrite.io/v1')
+.setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!)
+
+const database = new Databases(client);
+
+export const updateSearchCount = async (query: string, movie: Movie) => {
+    // check if a record of that search hase been stored
+
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+        Query.equal('searchTerm', query);
+    ])
+
+    console.log(result);
+}
