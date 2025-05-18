e74bf608000d5136533ad1969a0ac68ff4462540
nouribram <nourtesla@yahoo.com>
2025-05-18 13:54:47 +0300

    new update

--- movie-app/app/(tabs)/search.tsx
+++ movie-app/app/(tabs)/search.tsx
@@ -1,10 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, FlatList } from 'react-native'
 import React from 'react'
+import MovieCard from '@/components/MovieCard'
+import {images} from "@/constants/images";
 
 const search = () => {
+
+  
   return (
-    <View>
-      <Text>search</Text>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="flex-1 absolute w-full z-0" resizeMode="cover" />
+   
+        <FlatList 
+        data={movies}
+         renderItem={({ item }) => <MovieCard { ...item} />} 
+         />
     </View>
   )
 }
