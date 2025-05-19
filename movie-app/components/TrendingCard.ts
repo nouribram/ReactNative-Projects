45292b1aa598e4cb3f56bf616e0277ee54dade69
nouribram <nourtesla@yahoo.com>
2025-05-19 11:08:35 +0300

    new

--- movie-app/components/TrendingCard.ts
+++ movie-app/components/TrendingCard.ts
@@ -1,11 +1,23 @@
-import { View, Text } from 'react-native'
+import { View, Text, Touchable } from 'react-native'
 import React from 'react'
 
-const TrendingCard = () => {
+const TrendingCard = ( { movie: { movie_id, title, poster_url 
+
+}, index}: TrendingCardProps) => {
   return (
-    <View>
-      <Text>TrendingCard</Text>
-    </View>
+   <Link href={`/movies/${movie_id}`} asChild>
+     <TouchableOpacity className="w-32 relative pl-5">
+        <Image
+          source={{ uri: poster_url }}
+          className="w-32 h-48 rounded-lg"
+          resizeMode="cover"
+        />
+
+        <View className="absolute bottom-9 -left-3.5 px-2 py-1 rounded-full">
+        
+        </View>
+     </TouchableOpacity>
+   </Link>
   )
 }
 
