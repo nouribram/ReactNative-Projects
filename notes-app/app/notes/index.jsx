07d46f76484f37764955b3c08b129f19d72579c0
nouribram <nourtesla@yahoo.com>
2025-06-03 20:28:10 +0300

    new

--- notes-app/app/notes/index.jsx
+++ notes-app/app/notes/index.jsx
@@ -13,6 +13,20 @@ const NoteScreen = () => {
     const [modalVisible, setModalVisible] = useState(false);
     const [newNote, setNewNote] = useState('');
 
+    // Add new note
+   
+    const addNote = () => {
+        if(newNote.trim() === '') return;
+
+        setNotes((prevNotes) => [
+            ...prevNotes,
+            { id: Date.now.toString(), text: newNote }
+        ]);
+
+        setNewNote('');
+        setModalVisible(false);
+    }
+
     return( 
     <View style={styles.container}>
         <FlatList 
@@ -35,7 +49,26 @@ const NoteScreen = () => {
           <View style={styles.modalOverlay}>
              <View style={styles.modalContent}>
                 <Text style={styles.modalTitle}>Add a new note</Text>
-                <TextInput />
+                <TextInput 
+                  style={styles.input}
+                  placeholder='Enter note ..'
+                  placeholderTextColor='#aaa'
+                  value={newNote}
+                  onChangeText={setNewNote}
+                />
+                <View style={styles.modalButtons}>
+                    <TouchableOpacity style={styles.cancelButton} 
+                    onPress={ () => setModalVisible(false)}>
+
+                        <Text style={styles.cancelButtonText}>Cancel</Text>
+                    </TouchableOpacity>
+
+                      <TouchableOpacity style={styles.saveButton} 
+                    onPress={addNote}>
+
+                        <Text style={styles.saveButtonText}>Save</Text>
+                    </TouchableOpacity>
+                </View>
              </View>
           </View>
         </Modal>
@@ -74,6 +107,59 @@ const styles = StyleSheet.create ({
         color: '#fff',
         fontSize: 18,
         fontWeight: 'bold',
+    },
+    modalOverlay: {
+        flex: 1,
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    modalContent: {
+        backgroundColor: '#fff',
+        padding: 20,
+        borderRadius: 10,
+        width: '80%',
+    },
+    modalTitle: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 10,
+        textAlign: 'center',
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 8,
+        padding: 10,
+        fontSize: 16,
+        marginBottom: 15,
+    },
+    modalButtons: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    cancelButton: {
+        backgroundColor: '#ccc',
+        padding: 10,
+        borderRadius: 5,
+        flex: 1,
+        marginRight: 10,
+        alignItems: 'center',
+    },
+    cancelButtonText: {
+        fontSize: 16,
+        color: '#333',
+    },
+    saveButton: {
+        backgroundColor: '#007bff',
+        padding: 10,
+        borderRadius: 5,
+        flex: 1,
+        alignItems: 'center',
+    },
+    saveButtonText: {
+        fontSize: 16,
+        color: '#fff',
     },
 });
 
