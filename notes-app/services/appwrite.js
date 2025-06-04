ca0f34315c389c768101505820661e00b3418c5e
nouribram <nourtesla@yahoo.com>
2025-06-04 09:17:46 +0300

    new

---
+++ notes-app/services/appwrite.js
@@ -0,0 +1,28 @@
+import {Client, Database} from "react-native-appwrite"; 
+import { Platform } from "react-native";
+
+
+const config = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    db: process.env.EXPO_PUBLIC_APPWRITE_DB_ID,
+    col: {
+        notes: process.env.EXPO_PUBLIC_APPWRITE_COL_NOTES_ID
+    }
+};
+
+const client = new Client()
+
+.setEndPoint(config.endpoint)
+.setProject(config.projectId);
+
+switch(Platform.OS) {
+    case 'android': 
+    client.setPlatform(process.env.EXPO_PUBLIC_APPWRITE_PACLAGE_NAME);
+}
+
+
+const database = new Databases(client);
+
+
+export { database, config, client };
---
+++ notes-app/services/databaseService.js
@@ -0,0 +1,5 @@
+import {database} from './appwrite';
+
+const databaseService = {
+    
+}
