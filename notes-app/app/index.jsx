46585327950c6e34dacddf7b63bb05301db6bd17
nouribram <nourtesla@yahoo.com>
2025-08-05 22:35:34 +0300

    .

--- notes-app/app/index.jsx
+++ notes-app/app/index.jsx
@@ -26,7 +26,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    padding: 20,
+    padding: 18,
     backgroundColor: '#f8f9fa'
   },
   image: {
